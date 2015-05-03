d96475b8ff8b419886b6082ba36d4561c7190f50
kpendic <kpendic@gmail.com>
2015-05-03 10:47:34 +0200

    tutor test array auth

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var app = angular.module("sampleApp", ["firebase"]);
 app.controller("SampleCtrl", function($scope, $firebaseArray) {
-  var ref = new Firebase("https://angularfiretesting.firebaseio.com/data");
-  // create a synchronized array
-  $scope.messages = $firebaseArray(ref);
-  // add new items to the array
-  // the message is automatically added to Firebase!
-  $scope.addMessage = function() {
-    $scope.messages.$add({
-      text: $scope.newMessageText
-    });
-  };
-  // click on `index.html` above to see $remove() and $save() in action
+	var ref = new Firebase("https://angularfiretesting.firebaseio.com/data");
+
+	var auth = $firebaseAuth(ref);
+
+	auth.$authWithOAuthPopup("github").then(function(authData) {
+		console.log("Logged in as:", authData.uid);
+	}).catch(function(error) {
+		console.log("Authentication failed:", error);
+	});
+
+	// create a synchronized array
+	$scope.messages = $firebaseArray(ref);
+	// add new items to the array
+	// the message is automatically added to Firebase!
+	$scope.addMessage = function() {
+		$scope.messages.$add({
+			text: $scope.newMessageText
+		});
+	};
+	// click on `index.html` above to see $remove() and $save() in action
 });
