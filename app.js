7ff510fb0c7bab2f71db9b6b206062d5c987e994
kpendic <kpendic@gmail.com>
2015-05-03 17:50:19 +0200

    tutor test array auth google correct

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-// ==========> APP
 var app = angular.module("sampleApp", ["firebase"]);
-// ==========> CONTROLLER START
-app.controller("SampleCtrl", function( inst, fbauth ) {
-	var ref = new Firebase("https://torid-heat-8150.firebaseio.com");
-
-	var auth = fbauth(ref);
-
-	auth.$authWithOAuthPopup("google").then(function(authData) {
-		console.log("Logged in as:", authData );
-	}).catch(function(error) {
-		console.log("Authentication failed:", error);
-	});
-
-	// create a synchronized array
-	 inst.messages = $firebaseArray(ref);
-	// add new items to the array
-	// the message is automatically added to Firebase!
-	 inst.addMessage = function() {
-		 inst.messages.$add({
-			text:  inst.newMessageText
-		});
-	};
+app.controller("SampleCtrl", function($scope, $firebaseArray) {
+  var ref = new Firebase("https://angularfiretesting.firebaseio.com/data");
+  // create a synchronized array
+  $scope.messages = $firebaseArray(ref);
+  // add new items to the array
+  // the message is automatically added to Firebase!
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
+  // click on `index.html` above to see $remove() and $save() in action
 });
-// ==========> CONTROLLER END 
