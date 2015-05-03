02fa76f30412d64752b98dd35a30cb971365a608
kpendic <kpendic@gmail.com>
2015-05-03 09:32:00 +0200

    little redirect change from popup

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 (function(){
 
+	var app = angular.module( 'main', [] ) ;
+	app.controller( 'usersctrl', function( scope, fbobj ){
 
-	var ref = new Firebase("https://torid-heat-8150.firebaseio.com");
-		ref.authWithOAuthPopup("github", function(error, authData) {
-		if (error) {
-			console.log("Login Failed!", error);
-		} else {
-			console.log("Authenticated successfully with payload:", authData);
-		}
+		var ref = new Firebase("https://torid-heat-8150.firebaseio.com");
+			ref.authWithOAuthRedirect("github", function(error, authData) {
+			if (error) {
+				console.log("Login Failed!", error);
+			} else {
+				console.log("Authenticated successfully with payload:", authData);
+			}
+		});
+
+		var syncobj = fbobj( ref );
+		syncobj.$bindTo( scope, "data" );
+
 	});
-
-
-	var app = angular.module( 'main', [ ] ) ;
-	app.controller( 'usersctrl', function(){
-		this.user = gem;
-	});
-
-	var gem = [
-		{ name: 'mirko', DOB: '1/2/1980', canP: true },
-		{ name: 'slavko', DOB: '2/6/1988', canP: false },
-	];
 
 })();
